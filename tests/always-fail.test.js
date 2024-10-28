4b40c8eef3b3d7aab41632c55aade3535e27ec04
jaX10bt <132996313+jaX10bt@users.noreply.github.com>
2024-10-28 16:14:21 +0200

    Update always-fail.test.js

--- tests/always-fail.test.js
+++ tests/always-fail.test.js
@@ -1,6 +1,11 @@
 const { test, expect } = require('@playwright/test');
 
-test('this test should fail every time', async ({ page }) => {
+test('this test should fail every time 1', async ({ page }) => {
   await page.goto('https://example.com');
   expect(1).toBe(2);
+});
+
+test('this test should fail every time 2', async ({ page }) => {
+  await page.goto('https://example.com');
+  expect(3).toBe(6);
 });
