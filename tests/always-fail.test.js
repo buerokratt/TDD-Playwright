0482def8aba38e17b689f7f984e0e0d8a2158017
jaX10bt <132996313+jaX10bt@users.noreply.github.com>
2024-10-30 10:48:55 +0200

    Update always-fail.test.js

--- tests/always-fail.test.js
+++ tests/always-fail.test.js
@@ -1,11 +1,21 @@
 const { test, expect } = require('@playwright/test');
 
-test('this test should fail every time 1', async ({ page }) => {
+test('this test should fail every time 1', {
+  annotation: {
+    type: 'repository',
+    description: 'GH-API-Test-1'
+  },
+}, async ({ page }) => {
   await page.goto('https://example.com');
   expect(1).toBe(2);
 });
 
-test('this test should fail every time 2', async ({ page }) => {
+test('this test should fail every time 2', {
+  annotation: {
+    type: 'repository',
+    description: 'GH-API-Test-2'
+  },
+}, async ({ page }) => {
   await page.goto('https://example.com');
   expect(3).toBe(6);
 });
