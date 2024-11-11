4064657ef850ee1859e1cf8ec8856e5f27e58ac0
danyil <danyil.kurbatov@gmail.com>
2024-11-11 13:51:23 +0200

    Updated tests with generated

--- tests/admin/landingPage/mainPage/sideBar/administration/chatbot/welcome_message/visibility.spec.js
+++ tests/admin/landingPage/mainPage/sideBar/administration/chatbot/welcome_message/visibility.spec.js
@@ -1,90 +1,43 @@
 import { test, expect } from '@playwright/test';
-import { getTranslations } from '../../../../../../../translations/languageDetector';
+import { getTranslations } from '@translation/languageDetector.js';
 
-test.describe('Welcome message/Tervitussõnum Visibility Tests', () => {
+let translation;
 
-    let translation;
+test.describe('Settings - Welcome Message', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://admin.prod.buerokratt.ee/chat/chatbot/welcome-message');
+    translation = await getTranslations(page);
+    await page.waitForTimeout(3000);
+  });
 
-    test.beforeEach(async ({ page }) => {
-        test.info().annotations.push({ type: 'repository', description: 'Buerokratt-Chatbot' });
-        
-        await page.goto('https://admin.prod.buerokratt.ee/chat/chatbot/welcome-message');
-        translation = await getTranslations(page);
+  test('Heading verification', async ({ page }) => {
+    const heading = await page.getByRole('heading', { name: `${translation.welcomeMessage}` });
+    await expect(heading).toBeVisible();
+  });
+
+  test.describe('Card Body', () => {
+    test('Greeting Active switch visibility', async ({ page }) => {
+      const container = page.locator('.card');
+      const label = container.getByText(`${translation.greetingActive}`);
+      const switchButton = container.locator(`label:has-text("${translation.greetingActive}") + button.switch__button`);
+      await expect(label).toBeVisible();
+      await expect(switchButton).toBeVisible();
     });
 
-    test('Check visibility of the main title', async ({ page }) => {
-        // Check if the main title is visible
-        const mainTitle = page.locator(`h1:has-text("${translation["welcomeMessage"]}")`);
-        await expect(mainTitle).toBeVisible();
+    test('Welcome Message textarea visibility', async ({ page }) => {
+      const container = page.locator('.card');
+      const label = container.getByText(`${translation.welcomeMessage}`);
+      const textarea = container.getByLabel(`${translation.welcomeMessage}`);
+      await expect(label).toBeVisible();
+      await expect(textarea).toBeVisible();
     });
+  });
 
-    test('Check if the textarea is visible and contains text', async ({ page }) => {
-        // Locate the textarea within its surrounding structure
-        const textarea = page.locator('.textarea__wrapper textarea');
-    
-        // Check if the textarea is visible
-        await expect(textarea).toBeVisible();
-    
-        // Retrieve the value from the textarea
-        const textareaValue = await textarea.inputValue();
-    
-        // Check if the textarea contains any text
-        await expect(textareaValue).not.toBe('');
+  test.describe('Card Footer', () => {
+    test('Save button visibility', async ({ page }) => {
+      const container = page.locator('.card__footer');
+      const saveButton = container.getByRole('button', { name: `${translation.save}` });
+      await expect(saveButton).toBeVisible();
     });
-
-    test('Check visibility of the switch label "Tervitus aktiivne"', async ({ page }) => {
-        // Check if the switch label "Tervitus aktiivne" is visible
-        const switchLabel = page.locator(`label:has-text("${translation["greetingActive"]}")`);
-        await expect(switchLabel).toBeVisible();
-    });
-
-    test('Check visibility of the switch button', async ({ page }) => {
-        const switchButton = page.locator('.switch__button').nth(1);
-        await expect(switchButton).toBeVisible();
-    });
-
-    test('Check visibility of the textarea label', async ({ page }) => {
-        // Check if the textarea label is visible
-        const textareaLabel = page.locator(`label:has-text("${translation["welcomeMessage"]}")`);
-        await expect(textareaLabel).toBeVisible();
-    });
-
-    test('Check if the textarea contains text', async ({ page }) => {
-        // Locate the correct textarea using XPath with translation
-        const textarea = page.locator(`//label[text()="${translation["welcomeMessage"]}"]/following-sibling::div//textarea`);
-
-        // Retrieve the value from the textarea
-        const textareaValue = await textarea.inputValue();
-
-        // Check if the textarea contains any text
-        await expect(textareaValue).not.toBe('');
-    });
-
-    test.skip('Check if the character count reflects the length of the message ### LOOK ISSUE INSIDE', async ({ page }) => {
-
-        test.info().annotations.push({
-            type: 'Known bug',
-            description: 'It doesnt display the character count on first load',
-        })
-        // Locate the correct textarea using XPath with translation
-        const textarea = page.locator(`//label[text()="${translation["welcomeMessage"]}"]/following-sibling::div//textarea`);
-
-        // Retrieve the value from the textarea
-        const textareaValue = await textarea.inputValue();
-
-        // Verify the character count reflects the length of the message
-        const charCount = page.locator('.textarea__max-length-bottom');
-        await expect(charCount).toBeVisible();
-
-        // Verify that the character count matches the length of the message
-        const messageLength = textareaValue.length;
-        await expect(charCount).toHaveText(`${messageLength}/250`);
-    });
-
-    test('Check visibility of the "Salvesta" button', async ({ page }) => {
-        // Check if the "Salvesta" button is visible
-        const saveButton = page.locator(`button.btn--primary:has-text("${translation["save"]}")`);
-        await expect(saveButton).toBeVisible();
-    });
-
+  });
 });
