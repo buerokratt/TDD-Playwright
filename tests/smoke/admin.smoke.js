04c55511e3a9cede54dfce2e85fcfbecd3f8bba6
FM\rait.avastu <rait.avastu@finest.eu>
2025-11-07 12:07:30 +0200

    updated smoke tests to use correct assertions

--- tests/smoke/admin.smoke.js
+++ tests/smoke/admin.smoke.js
@@ -3,40 +3,40 @@ import { URLS } from '../../playwright.config';
 
 test('Users Page', async({ page }) => {
     await page.goto(URLS.admin + 'chat/users' );
-    await page.getByRole('button', {name: 'Lisa kasutaja'}).waitFor({ state: 'visible' })
+    await expect(page.getByRole('button', {name: 'Lisa kasutaja'})).toBeVisible();
 });
 
 test('Settings Page', async({ page }) => {
     await page.goto(URLS.admin + 'chat/chatbot/settings' );
-    await page.getByLabel('Vestlusrobot aktiivne').waitFor({ state: 'visible' })
+    await expect(page.getByLabel('Vestlusrobot aktiivne')).toBeVisible();
 });
 
 test('Welcome message Page', async({ page }) => {
     await page.goto(URLS.admin + 'chat/chatbot/welcome-message' );
-    await page.getByLabel('Tervitus aktiivne').waitFor({ state: 'visible' })
+    await expect(page.getByLabel('Tervitus aktiivne')).toBeVisible();
 });
 
 test('Appearance Page', async({ page }) => {
     await page.goto(URLS.admin + 'chat/chatbot/appearance' );
-    await page.getByRole('switch', {name: 'Märguandesõnum'}).waitFor({ state: 'visible' })
+    await expect(page.getByRole('switch', {name: 'Märguandesõnum'})).toBeVisible();
 });
 
 test('Emergency message Page', async({ page }) => {
     await page.goto(URLS.admin + 'chat/chatbot/emergency-notices' );
-    await page.getByLabel('Teade aktiivne').waitFor({ state: 'visible' })
+    await expect(page.getByLabel('Teade aktiivne')).toBeVisible();
 });
 
 test('Feedback Page', async({ page }) => {
     await page.goto(URLS.admin + 'chat/chatbot/feedback' );
-    await page.getByLabel('Tagasiside aktiivne').waitFor({ state: 'visible' })
+    await expect(page.getByLabel('Tagasiside aktiivne')).toBeVisible();
 });
 
 test('Working time Page', async({ page }) => {
     await page.goto(URLS.admin + 'chat/working-time' );
-    await page.getByLabel('Kasutan klienditeenindust').waitFor({ state: 'visible' })
+    await expect(page.getByLabel('Kasutan klienditeenindust')).toBeVisible();
 });
 
 test('Session lenght Page', async({ page }) => {
     await page.goto(URLS.admin + 'chat/session-length' );
-    await page.getByLabel('Sessiooni pikkus').waitFor({ state: 'visible' })
+    await expect(page.getByLabel('Sessiooni pikkus')).toBeVisible();
 });
