e0ad55613f4ad01bf136c22f59037bbe84617597
sherifbai <ruzmetov_sh@tuneconsulting.net>
2026-06-25 17:58:57 +0500

    update prettier & eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,51 @@
+const tseslint = require('typescript-eslint');
+const prettierRecommended = require('eslint-plugin-prettier/recommended');
+const unusedImports = require('eslint-plugin-unused-imports');
+const importPlugin = require('eslint-plugin-import');
 const globals = require('globals');
 
 module.exports = [
   {
-    ignores: [
-      'node_modules/**',
-      'playwright-report/**',
-      'test-results/**',
-    ],
+    ignores: ['**/*.js', 'node_modules/**', 'playwright-report/**', 'test-results/**'],
   },
+
+  ...tseslint.configs.recommended,
+
   {
-    files: ['**/*.js'],
+    files: ['**/*.ts'],
     languageOptions: {
       ecmaVersion: 'latest',
-      sourceType: 'commonjs',
-      globals: {
-        ...globals.node,
-      },
+      sourceType: 'module',
+      globals: { ...globals.node },
+    },
+    plugins: {
+      'unused-imports': unusedImports,
+      import: importPlugin,
     },
     rules: {
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      'no-undef': 'error',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
     },
   },
+
+  prettierRecommended,
 ];
